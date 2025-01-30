2c1001d6b3c696d257bf4d70f7218069298fe1ca
Lilac <107518574+Satellaa@users.noreply.github.com>
2025-01-30 15:13:54 +0700

    Update "Gishki Photomirror"

--- official/c9236985.lua
+++ official/c9236985.lua
@@ -1,10 +1,12 @@
 --リチュアの写魂鏡
 --Gishki Photomirror
 --Modified for CrimsonAlpha
+Duel.LoadScript("_load_.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    --Ritual Summon any "Gishki" Ritual Monster
     local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
@@ -17,50 +19,32 @@ function s.initial_effect(c)
     s.ritual_matching_function[c]=aux.FilterEqualFunction(Card.IsSetCard,SET_GISHKI)
 end
 s.listed_series={SET_GISHKI}
--- custom --
-function s.extralocfilter(c,e,tp,lp)
-    if not c:IsRitualMonster() 
-	or not c:IsSetCard(SET_GISHKI) 
-	or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) 
-	then
-        return false
-    end
-	local extra_loc_eff,used=Ritual.ExtraLocationOPTCheck(c,e:GetHandler(),tp)
-	if not extra_loc_eff or extra_loc_eff and used then return false end
-	if extra_loc_eff:GetProperty()&EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN>0 
-	and Duel.HasFlagEffect(tp,EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN) then 
-		return false 
+function s.ritualfilter(c,e,tp,lp)
+    if not c:IsRitualMonster() or not c:IsSetCard(SET_GISHKI) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
+    if not c:IsLocation(LOCATION_HAND) then
+		local extra_loc_eff,used=Ritual.ExtraLocationOPTCheck(c,e:GetHandler(),tp)
+		if not extra_loc_eff or extra_loc_eff and used then return false end
+		if extra_loc_eff:GetProperty()&EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN>0 and Duel.HasFlagEffect(tp,EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN) then 
+			return false 
+		end
 	end
 	return lp>c:GetLevel()*500
 end
-function s.filter(c,e,tp,lp)
-    if not c:IsRitualMonster() or not c:IsSetCard(SET_GISHKI) 
-	or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then
-        return false
-    end
-    return lp>c:GetLevel()*500
-end
---
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
         local lp=Duel.GetLP(tp)
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-            and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,lp)
-			-- custom --
-			 or Duel.IsExistingMatchingCard(s.extralocfilter,tp,LOCATION_NOTHAND,0,1,nil,e,tp,lp)
-			--
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.ritualfilter,tp,LOCATION_HAND|LOCATION_NOTHAND,0,1,nil,e,tp,lp)
     end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_NOTHAND)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local lp=Duel.GetLP(tp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local lp=Duel.GetLP(tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	-- custom --
-	local tg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil,e,tp,lp)
-	local extratg=Duel.GetMatchingGroup(s.extralocfilter,tp,LOCATION_NOTHAND,0,nil,e,tp,lp)
-	tg=tg:Merge(extratg)
-    local tc=tg:Select(tp,1,1,nil):GetFirst()
+	local tg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.ritualfilter),tp,LOCATION_HAND|LOCATION_NOTHAND,0,nil,e,tp,lp)
+	local tc=tg:Select(tp,1,1,nil):GetFirst()
+	if not tc then return end
 	local extra_loc_eff=Ritual.GetExtraLocationEffect(tc,e:GetHandler())
 	if extra_loc_eff and extra_loc_eff:CheckCountLimit(tp) then
 		local extra_loc=extra_loc_eff:GetTargetRange()
@@ -72,12 +56,10 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 	--
-    if tc then
-        mustpay=true
-        Duel.PayLPCost(tp,tc:GetLevel()*500)
-        mustpay=false
-        tc:SetMaterial(nil)
-        Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-        tc:CompleteProcedure()
-    end
+	mustpay=true
+	Duel.PayLPCost(tp,tc:GetLevel()*500)
+	mustpay=false
+	tc:SetMaterial(nil)
+	Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+	tc:CompleteProcedure()
 end
