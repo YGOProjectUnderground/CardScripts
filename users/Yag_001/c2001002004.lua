e1502a570fb24d54639aafc510dca5c1c3483ee1
YoRepo <51199752+YoRepo@users.noreply.github.com>
2025-03-01 22:50:54 +0200

    fixed c2001002004 - Needle of Spun Fates

--- users/Yag_001/c2001002004.lua
+++ users/Yag_001/c2001002004.lua
@@ -3,14 +3,16 @@
 Duel.LoadScript("_load_.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
+    --Ritual Summon from Deck or GY
+    local e1=Ritual.CreateProc({
+        handler=c,
+        lvtype=RITPROC_EQUAL,
+        filter=aux.FilterBoolFunction(Card.IsCode,2001002001,2001002002,2001002003),
+        extrafil=s.extragroup,
+        extraop=s.extraop,
+        location=LOCATION_DECK|LOCATION_GRAVE,
+        stage2=s.stage2
+    })
     c:RegisterEffect(e1)
     
     --Return to deck
@@ -27,36 +29,10 @@ function s.initial_effect(c)
 end
 
 s.listed_names={2001002001,2001002002,2001002003} --Divine Weaver monsters IDs
-s.listed_series={0x79b}
 
---Helper function for ritual requirements (adapted from Divine Weaver of Genesis)
-local function RitualCheck(sc,lv,forcedselection,_type,requirementfunc)
-    local chk
-    if _type==RITPROC_EQUAL then
-        chk=function(g) return g:GetSum(requirementfunc or Card.GetRitualLevel,sc)>=lv end
-    else
-        chk=function(g,c) return g:GetSum(requirementfunc or Card.GetRitualLevel,sc) - (requirementfunc or Card.GetRitualLevel)(c,sc)>=lv end
-    end
-    return function(sg,e,tp,mg,c)
-        local res=chk(sg,c)
-        if not res then return false,true end
-        local stop=false
-        if forcedselection then
-            local ret=forcedselection(e,tp,sg,sc)
-            res=ret[1]
-            stop=ret[2] or stop
-        end
-        if res and not stop then
-            if _type==RITPROC_EQUAL then
-                res=sg:CheckWithSumEqual(requirementfunc or Card.GetRitualLevel,lv,#sg,#sg,sc)
-            else
-                Duel.SetSelectedCard(sg)
-                res=sg:CheckWithSumGreater(requirementfunc or Card.GetRitualLevel,lv,sc)
-            end
-            res=res and Duel.GetMZoneCount(tp,sg,tp)>0
-        end
-        return res,stop
-    end
+--Extra materials from GY that can be banished
+function s.extragroup(e,tp,eg,ep,ev,re,r,rp,chk)
+    return Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil)
 end
 
 --Filter for monsters in GY that can be banished as material
@@ -64,37 +40,12 @@ function s.matfilter(c)
     return c:IsMonster() and c:HasLevel() and c:IsAbleToRemove()
 end
 
---Filter for valid ritual monsters
-function s.ritualfilter(c,e,tp,m,m2)
-    if not (c:IsCode(2001002001,2001002002,2001002003) and c:IsRitualMonster()) then return false end
-    if not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-    
-    local lv=c:GetLevel()
-    local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-    
-    -- If checking a card in GY, don't include it in material calculation
-    local mg2_filtered = m2:Clone()
-    if c:IsLocation(LOCATION_GRAVE) then
-        mg2_filtered:RemoveCard(c)
-    end
-    mg:Merge(mg2_filtered)
-    
-    if c.mat_filter then
-        mg=mg:Filter(c.mat_filter,nil,tp)
-    end
-    
-    return aux.SelectUnselectGroup(mg,e,tp,1,99,RitualCheck(c,lv,nil,RITPROC_EQUAL),0)
-end
-
---Target function for ritual summoning
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local mg=Duel.GetRitualMaterial(tp)
-        local mg2=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil)
-        
-        return Duel.IsExistingMatchingCard(s.ritualfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp,mg,mg2)
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+--Process banished materials
+function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+    local mat2=mat:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+    mat:Sub(mat2)
+    Duel.ReleaseRitualMaterial(mat)
+    Duel.Remove(mat2,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
 end
 
 --Set appropriate Spell/Trap after Ritual Summon
@@ -108,64 +59,16 @@ function s.settfilter(c,code)
     return c:IsCode(codes[code]) and c:IsSSetable()
 end
 
---Activation operation
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local mg=Duel.GetRitualMaterial(tp)
-    local mg2=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil)
+--After Ritual Summon, set corresponding card
+function s.stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+    if not tc or Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
     
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local tg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.ritualfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp,mg,mg2)
-    
-    if #tg>0 then
-        local tc=tg:GetFirst()
-        local lv=tc:GetLevel()
-        
-        -- Create material pool
-        local mgf=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-        
-        -- Remove the specific ritual monster from GY materials if it's being summoned from GY
-        local mg2_filtered = mg2:Clone()
-        if tc:IsLocation(LOCATION_GRAVE) then
-            mg2_filtered:RemoveCard(tc)
-        end
-        mgf:Merge(mg2_filtered)
-        
-        if tc.mat_filter then
-            mgf=mgf:Filter(tc.mat_filter,nil,tp)
-        end
-        
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-        mat=aux.SelectUnselectGroup(mgf,e,tp,1,99,RitualCheck(tc,lv,nil,RITPROC_EQUAL),1,tp,HINTMSG_RELEASE)
-        
-        if #mat>0 then
-            tc:SetMaterial(mat)
-            
-            --Process materials from different locations
-            local mat_gy=mat:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
-            local mat_field=mat-mat_gy
-            
-            if #mat_gy>0 then
-                Duel.Remove(mat_gy,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
-            end
-            if #mat_field>0 then
-                Duel.Release(mat_field,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
-            end
-            
-            Duel.BreakEffect()
-            --Perform the ritual summon
-            if Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)>0 then
-                --Check if we should set a spell/trap
-                if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-                    local g=Duel.GetMatchingGroup(s.settfilter,tp,LOCATION_DECK,0,nil,tc:GetCode())
-                    if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-                        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-                        local sg=g:Select(tp,1,1,nil)
-                        if #sg>0 then
-                            Duel.SSet(tp,sg)
-                        end
-                    end
-                end
-            end
+    local g=Duel.GetMatchingGroup(s.settfilter,tp,LOCATION_DECK,0,nil,tc:GetCode())
+    if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+        local sg=g:Select(tp,1,1,nil)
+        if #sg>0 then
+            Duel.SSet(tp,sg)
         end
     end
 end
