54049f9ea9c3547f1a1dda3529c6a9fe53636313
DuDono <62557649+DuDono@users.noreply.github.com>
2025-04-16 22:00:52 +0200

    Fix cards returning 1 by 1
    Might have caused other fixes, not sure.

--- users/DuDono_004/c2004000032.lua
+++ users/DuDono_004/c2004000032.lua
@@ -25,23 +25,42 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,0,LOCATION_SZONE+LOCATION_GRAVE,nil)
 	local c=e:GetHandler()
+	local fid=c:GetFieldID()
 	local d=0
 	for tc in aux.Next(g) do
 		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetCode(EVENT_PHASE+PHASE_END)
-			e1:SetRange(LOCATION_REMOVED)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			e1:SetCountLimit(1)
-			e1:SetOperation(s.retop)
-			tc:RegisterEffect(e1)
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,fid)
 			d=d+1
 		end
 	end
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetLabel(fid)
+	e2:SetLabelObject(g)
+	e1:SetCondition(s.retcon)
+	e1:SetOperation(s.retop)
+	Duel.RegisterEffect(e1,tp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
+function s.retfilter(c,fid)
+	return c:GetFlagEffectLabel(id)==fid
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT+REASON_RETURN)
+	local g=e:GetLabelObject()
+	local dg=g:Filter(s.retfilter,nil,e:GetLabel())
+	g:DeleteGroup()
+	Duel.SendtoGrave(dg,REASON_EFFECT+REASON_RETURN)
 end
