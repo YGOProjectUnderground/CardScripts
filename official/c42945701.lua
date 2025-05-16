3cba81cf9cdf74b261c0e85fb562ed93808b1303
Satellaa <nguyendinhtrong1009@gmail.com>
2025-05-16 14:17:17 +0700

    Removed `Duel.LoadScript("_load_.lua")`
    because now `init.lua` is working in the repository

--- official/c42945701.lua
+++ official/c42945701.lua
@@ -1,5 +1,4 @@
 --Fire Spirit Art - Kurenai
-Duel.LoadScript("_load_.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
--- official/c79333300.lua
+++ official/c79333300.lua
@@ -1,5 +1,4 @@
 --Wind Spirit Art - Miyabi
-Duel.LoadScript("_load_.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
